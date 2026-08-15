be9e539b70b97f7527176a0baf8d26bddfd178cf
occult-kranti <2.95667039e+08+occult-kranti@users.noreply.github.com>
2026-08-15 20:47:54 +0000

    Exclude local test dependencies from extension

--- extensions/wizardforums-scraper/tests/debug_login.js
+++
@@ -1,6 +0,0 @@
-const { parseHTML } = require('linkedom');
-const XF = require('../lib/xf-parse.js');
-for (const html of ['<body>You must log in or register to view this page</body>', '<html><body>You must log in or register to view this page</body></html>']) {
-  const d = parseHTML(html).document;
-  console.log({html, documentHTML: d.toString(), bodyHTML: d.body && d.body.toString(), bodyText: d.body && d.body.textContent, wallDoc: XF.isLoginWall(d), wallBody: XF.isLoginWall(d.body)});
-}
